25fbc9f5cc4e43d0d91caf9fe90f0a6565975206
Mikhail Kulemin <mih@mrphs.cc>
2023-05-06 19:21:07 +0100

    Add gRPC server and client for expression calculation

---
+++ cmd/calc_client/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"strconv"
+	"strings"
+	"flag"
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/mhbvr/grpc-example/pkg/calc_protos"
+)
+
+var (
+	addr = flag.String("addr", "localhost:5050", "the address of Calc server")
+)
+
+func parseValue(s string) (string, float64, error) {
+	fields := strings.Split(s, "=")
+	if len(fields) != 2 {
+		return "", 0.0, fmt.Errorf("can not parse variable, %v")
+	}
+	value, err := strconv.ParseFloat(fields[1], 64)
+	if err != nil {
+		return "", 0.0, fmt.Errorf("can not parse float value, %v", fields[1])
+	}
+
+	if len(fields[0]) == 0 {
+		return "", 0.0, fmt.Errorf("empty variable name")
+	}
+	return fields[0], value, nil
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		log.Fatalf("Need to provide an expression for evaluation")
+	}
+
+	request := &pb.ComputeRequest{Expression: flag.Args()[0]}
+
+	vars := make([]*pb.Variable,0)
+
+	for _, s := range flag.Args()[1:] {
+		variable, value, err := parseValue(s)
+		if err != nil {
+			log.Fatalf("incorrect variable, %v", err)
+		}
+		vars = append(vars, &pb.Variable{Name:variable, Value: value})
+	}
+
+	request.Vars = vars
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewCalcClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Compute(ctx, request)
+	if err != nil {
+		log.Fatalf("could not compute: %v", err)
+	}
+
+	fmt.Printf("Result: %v\n", r.GetResult())
+}
---
+++ cmd/calc_server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"flag"
+	"net"
+	"context"
+
+	"github.com/mhbvr/grpc-example/pkg/eval"
+	"google.golang.org/grpc"
+	pb "github.com/mhbvr/grpc-example/pkg/calc_protos"
+)
+
+
+var (
+	port = flag.Int("port", 5050, "The server gRPC port")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedCalcServer
+}
+
+// Implementation of Compute RC
+func (s *server) Compute(ctx context.Context, in *pb.ComputeRequest) (*pb.ComputeReply, error) {
+	log.Printf("ComputeRequest: %v", in)
+
+	expr, err := eval.Parse(in.Expression)
+	if err != nil {
+		log.Printf("incorrect expression, %v", err)
+		return nil, err
+	}
+
+	var env eval.Env = make(map[eval.Var]float64)
+	for _, v := range in.Vars {
+		env[eval.Var(v.Name)] = v.Value
+	}
+
+	return &pb.ComputeReply{Result: expr.Eval(env)}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterCalcServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
